ae875c888a9ba9da077c14133781f8da8356e56a
traceybullington <traceylbullington@gmail.com>
2024-05-10 17:57:02 -0500

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Tracey Bullington on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
